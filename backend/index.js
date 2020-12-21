ac6fe36caca518031ed00a39eb7125b6ef5a0b85
Benedict Lau <b@deprecated.systems>
2020-12-21 03:15:13 -0500

    Add support for hyper protocol

--- backend/index.js
+++ backend/index.js
@@ -15,132 +15,136 @@ const period = conf['dev']['refreshPeriod'] ? conf['dev']['refreshPeriod'] : '0 
 // Run this job every minute by default
 const job = new cron.CronJob(period, function() {
   conf['activeProjects'].forEach((project, index) => {
-    let projFile = fs.readFileSync(`../data/${project}/config.json`);
-    let proj = JSON.parse(projFile);
-    let url = '';
+    try {
+      let projFile = fs.readFileSync(`../data/${project}/config.json`);
+      let proj = JSON.parse(projFile);
+      let url = '';
 
-    // Query monetization account balances
-    fetchPromises = [];
-    proj['monetization'].forEach((item, index) => {
-      switch (item['type']) {
-        case 'oc':
-          // Fetch Open Collective balance
-          url = `https://opencollective.com/${item['account']}.json`;
-          console.log(`GET ${url}`);
-          fetchPromises.push(fetch(url)
-            .then(res => res.json())
-            .then(json => {
-              return {
-                name: item['name'],
-                type: item['type'],
-                balances: [
-                  {
-                    balance: json['balance'].toString(),
-                    decimal: 2,
-                    currency: 'CAD'
-                  }
-                ]
-              }
-            })
-            .catch(function(error) {
-              console.log(error);
-            }));
-          break;
-        case 'eth':
-          // Fetch Ethereum address ETH balance
-          url = `https://api.etherscan.io/api?module=account&action=balance&address=${item['account']}&tag=latest&apikey=${conf['etherscanApikey']}`;
-          console.log(`GET ${url}`);
-          fetchPromises.push(fetch(url)
-            .then(res => res.json())
-            .then(json => {
-              if (json['status'] !== '1') throw new Error(`Failed to fetch from remote ${url}`);
-              return {
-                name: item['name'],
-                type: item['type'],
-                balances: [
-                  {
-                    balance: cropPrecision(json['result'], 12),
-                    decimal: 6,
-                    currency: 'ETH'
-                  }
-                ]
-              }
-            })
-            .catch(function(error) {
-              console.log(error);
-            }));
-          break;
-        case 'erc20':
-          // Fetch Ethereum address ERC20 balances
-          url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${item['account']}&startblock=0&endblock=999999999&sort=asc&apikey=${conf['etherscanApikey']}`;
-          console.log(`GET ${url}`);
-          fetchPromises.push(fetch(url)
-            .then(res => res.json())
-            .then(json => {
-              if (json['status'] !== '1') throw new Error(`Failed to fetch from remote ${url}`);
-              let tokenMap = new Map();
-              json['result'].forEach((tx, index) => {
-                if (tx['confirmations'] > 6) {
-                  let value = 0;
-                  let tokenDecimal = Number(tx['tokenDecimal']);
-
-                  // Crop to a max of 6-decimal precision
-                  if (tx['tokenDecimal'] > 6) {
-                    value = Number(cropPrecision(tx['value'], tokenDecimal - 6));
-                    tokenDecimal = 6;
-                  } else {
-                    value = Number(tx['value']);
-                  }
-
-                  // Make value negative if paid out from account
-                  if (equalsIgnoreCase(tx['from'], item['account'])) value = -value;
-
-                  // Add transaction to map
-                  if (tokenMap.has(tx['tokenSymbol'])) {
-                    let tokenBalance = tokenMap.get(tx['tokenSymbol']);
-                    if (tokenBalance['tokenDecimal'] !== tokenDecimal) throw new Error(`The token ${tokenSymbol} has transactions with different tokenDecimal`);
-                    let cumulativeValue = tokenBalance['value'] + value;
-                    tokenMap.set(tx['tokenSymbol'], { 'value': cumulativeValue, 'tokenDecimal': tokenDecimal });
-                  } else {
-                    tokenMap.set(tx['tokenSymbol'], { 'value': value, 'tokenDecimal': tokenDecimal });
-                  }
+      // Query monetization account balances
+      fetchPromises = [];
+      proj['monetization'].forEach((item, index) => {
+        switch (item['type']) {
+          case 'oc':
+            // Fetch Open Collective balance
+            url = `https://opencollective.com/${item['account']}.json`;
+            console.log(`GET ${url}`);
+            fetchPromises.push(fetch(url)
+              .then(res => res.json())
+              .then(json => {
+                return {
+                  name: item['name'],
+                  type: item['type'],
+                  balances: [
+                    {
+                      balance: json['balance'].toString(),
+                      decimal: 2,
+                      currency: 'CAD'
+                    }
+                  ]
                 }
-              });
+              })
+              .catch(function(error) {
+                console.log(error);
+              }));
+            break;
+          case 'eth':
+            // Fetch Ethereum address ETH balance
+            url = `https://api.etherscan.io/api?module=account&action=balance&address=${item['account']}&tag=latest&apikey=${conf['etherscanApikey']}`;
+            console.log(`GET ${url}`);
+            fetchPromises.push(fetch(url)
+              .then(res => res.json())
+              .then(json => {
+                if (json['status'] !== '1') throw new Error(`Failed to fetch from remote ${url}`);
+                return {
+                  name: item['name'],
+                  type: item['type'],
+                  balances: [
+                    {
+                      balance: cropPrecision(json['result'], 12),
+                      decimal: 6,
+                      currency: 'ETH'
+                    }
+                  ]
+                }
+              })
+              .catch(function(error) {
+                console.log(error);
+              }));
+            break;
+          case 'erc20':
+            // Fetch Ethereum address ERC20 balances
+            url = `https://api.etherscan.io/api?module=account&action=tokentx&address=${item['account']}&startblock=0&endblock=999999999&sort=asc&apikey=${conf['etherscanApikey']}`;
+            console.log(`GET ${url}`);
+            fetchPromises.push(fetch(url)
+              .then(res => res.json())
+              .then(json => {
+                if (json['status'] !== '1') throw new Error(`Failed to fetch from remote ${url}`);
+                let tokenMap = new Map();
+                json['result'].forEach((tx, index) => {
+                  if (tx['confirmations'] > 6) {
+                    let value = 0;
+                    let tokenDecimal = Number(tx['tokenDecimal']);
 
-              // Format result from tokenMap
-              let result = {
-                name: item['name'],
-                type: item['type'],
-                balances: []
-              };
-              tokenMap.forEach((mapValue, mapKey, map) => {
-                result['balances'].push({ balance: mapValue['value'].toString(), decimal: mapValue['tokenDecimal'], currency: mapKey });
-              });
-              return result;
-            })
-            .catch(function(error) {
-              console.log(error);
-            }));
-          break;
-        default:
-          throw err;
-      };
-    });
+                    // Crop to a max of 6-decimal precision
+                    if (tx['tokenDecimal'] > 6) {
+                      value = Number(cropPrecision(tx['value'], tokenDecimal - 6));
+                      tokenDecimal = 6;
+                    } else {
+                      value = Number(tx['value']);
+                    }
 
-    Promise.all(fetchPromises).then(values => {
-      // Write account balances to file
-      const balances = JSON.stringify({ 'result': values.filter(x => x), 'error': '', 'errorCode': 0 });
-      const dir = `../data/${project}/api/${apiVersion}/monetization/`;
-      if (!fs.existsSync(dir)) {
-        fs.mkdirSync(dir, { recursive: true });
-      }
-      fs.writeFile(`${dir}/balances.json`, balances, (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log(`balances.json updated for ${project}`);
+                    // Make value negative if paid out from account
+                    if (equalsIgnoreCase(tx['from'], item['account'])) value = -value;
+
+                    // Add transaction to map
+                    if (tokenMap.has(tx['tokenSymbol'])) {
+                      let tokenBalance = tokenMap.get(tx['tokenSymbol']);
+                      if (tokenBalance['tokenDecimal'] !== tokenDecimal) throw new Error(`The token ${tokenSymbol} has transactions with different tokenDecimal`);
+                      let cumulativeValue = tokenBalance['value'] + value;
+                      tokenMap.set(tx['tokenSymbol'], { 'value': cumulativeValue, 'tokenDecimal': tokenDecimal });
+                    } else {
+                      tokenMap.set(tx['tokenSymbol'], { 'value': value, 'tokenDecimal': tokenDecimal });
+                    }
+                  }
+                });
+
+                // Format result from tokenMap
+                let result = {
+                  name: item['name'],
+                  type: item['type'],
+                  balances: []
+                };
+                tokenMap.forEach((mapValue, mapKey, map) => {
+                  result['balances'].push({ balance: mapValue['value'].toString(), decimal: mapValue['tokenDecimal'], currency: mapKey });
+                });
+                return result;
+              })
+              .catch(function(error) {
+                console.log(error);
+              }));
+            break;
+          default:
+            throw err;
+        };
       });
-    });
+
+      Promise.all(fetchPromises).then(values => {
+        // Write account balances to file
+        const balances = JSON.stringify({ 'result': values.filter(x => x), 'error': '', 'errorCode': 0 });
+        const dir = `../data/${project}/api/${apiVersion}/monetization`;
+        if (!fs.existsSync(dir)) {
+          fs.mkdirSync(dir, { recursive: true });
+        }
+        fs.writeFile(`${dir}/balances.json`, balances, (err) => {
+          if (err) {
+            throw err;
+          }
+          console.log(`balances.json updated for ${project}`);
+        });
+      });
+    } catch (error) {
+      console.log(error);
+    }
   });
 }, null, true);
 
--- pinning-services/index.js
+++ pinning-services/index.js
@@ -1,6 +1,7 @@
 const cron = require('cron');
 const fetch = require('node-fetch');
 const fs = require('fs');
+const hyperPublisher = require('hyperdrive-publisher')
 const ipfsClient = require('ipfs-http-client')
 
 // Application constants
@@ -12,73 +13,134 @@ const confFile = fs.readFileSync(`../data/config.json`);
 const conf = JSON.parse(confFile);
 
 // Runtime settings
-const period = conf['dev']['pinningPeriod'] ? conf['dev']['pinningPeriod'] : '0 */10 * * * *'
+const period = conf['dev']['pinningPeriod'] ? conf['dev']['pinningPeriod'] : '0 */15 * * * *'
 const { globSource } = ipfsClient;
 const ipfs = ipfsClient(conf['ipfsServer']);
 
-// Run this job every 10 minutes by default
+// Connect to dat-store
+const DatStorageClient = require('dat-storage-client');
+const datStoreClient = new DatStorageClient(conf['datStore']['server']);
+console.log(`Connecting to dat-store at ${conf['datStore']['server']} ...`);
+datStoreClient.login(conf['datStore']['username'], conf['datStore']['password'])
+  .catch(function(error) {
+    console.log(error);
+  });
+
+// Run this job every 15 minutes by default
 const job = new cron.CronJob(period, function() {
   conf['activeProjects'].forEach((project, index) => {
-    let projFile = fs.readFileSync(`../data/${project}/config.json`);
-    let proj = JSON.parse(projFile);
-    let domain = proj['domain'];
+    try {
+      let projFile = fs.readFileSync(`../data/${project}/config.json`);
+      let proj = JSON.parse(projFile);
+      let domain = proj['domain'];
+      let dir = `../data/${project}/api`;
 
-    // Pin API responses to Hypercore
-    // updateDnsRecordDigitalOcean(domain, 'TXT', txtHypercore, `datkey=${hyperDriveKey}`, 300, conf['digitalOceanAccessToken']);
+      // Pin API responses to Hypercore
+      getDatSeed(project, domain)
+        .then(seed => hyperPublisher.sync({
+          seed,
+          fsPath: dir,
+          drivePath: '/'
+        }))
+        .then(({diff, url}) => {
+          // Log any changes to hyperdrive and return url
+          console.log(`API responses for ${project} pinned at ${url}. Changes:`);
+          console.log(diff);
+          return url;
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
 
-    // Pin API responses to IPFS
-    ipfs.add(globSource(`../data/${project}/api`, { pin: true, recursive: true, timeout: 10000 }))
-      .then(file => file['cid'].toV1().toString())
-      .then(cid => {
-        console.log(`API responses pinned at ipfs/${cid}`);
+      // Pin API responses to IPFS
+      ipfs.add(globSource(dir, { pin: true, recursive: true, timeout: 10000 }))
+        .then(file => file['cid'].toV1().toString())
+        .then(cid => {
+          console.log(`API responses for ${project} pinned at ipfs/${cid}`);
 
-        // Update DNS record
-        updateDnsRecordDigitalOcean(domain, 'TXT', txtIpfs, `dnslink=/ipfs/${cid}`, 300, conf['digitalOceanAccessToken']);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
+          // Update DNS record
+          return updateDnsRecordDigitalOcean(domain, 'TXT', txtIpfs, `dnslink=/ipfs/${cid}`, 300, conf['digitalOceanAccessToken']);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    } catch (error) {
+      console.log(error);
+    }
   });
 }, null, true);
 
-function updateDnsRecordDigitalOcean(domain, recordType, recordName, recordData, recordTtl, doToken) {
-   return new Promise((resolve, reject) => {
-    // List existing DNS records for domain
-    let url = `https://api.digitalocean.com/v2/domains/${domain}/records/`;
-    let headers = {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${doToken}`
-    };
-    console.log(`GET ${url}`);
-    fetch(url, { headers: headers })
-      .then(res => res.json())
-      .then(json => json['domain_records'].filter(record => record['type'] === recordType && record['name'] === recordName))
-      .then(txt => {
-        // Delete existing records with matching record type and name
-        txt.forEach((item, index) => {
-          let urlDelete = url + item['id'];
-          console.log(`DELETE ${urlDelete}`);
-          fetch(urlDelete, { method: 'DELETE', headers: headers })
-            .catch(function(error) {
-              console.log(error);
-            });
-        });
+async function getDatSeed(project, domain) {
+  // Read private dat-seed
+  let dirPrivate = `../data/${project}/private`
+  let seed;
+  try {
+    seed = fs.readFileSync(`${dirPrivate}/dat-seed`);
+  } catch (error) {
+    console.log('Project dat-seed not found');
+  }
+  // If no dat-seed is stored, generate new dat-seed and publish hyperdrive
+  if (seed === undefined) {
+    // Generate new dat-seed
+    console.log(`Generating new dat-seed ...`);
+    seed = require('crypto').randomBytes(32);
 
-        // Create new DNS record
-        data = JSON.stringify({
-          type: recordType,
-          name: recordName,
-          data: recordData,
-          ttl:  recordTtl
+    // Store new dat-seed in private directory of project
+    if (!fs.existsSync(dirPrivate)) {
+      fs.mkdirSync(dirPrivate, { recursive: true });
+    }
+    fs.writeFileSync(`${dirPrivate}/dat-seed`, seed);
+    console.log(`Project dat-seed updated for ${project}`);
+
+    // Publish hyperdrive to dat-store with new seed
+    await hyperPublisher.getURL({seed})
+      .then(url => datStoreClient.add(url))
+      .then(() => hyperPublisher.create({seed}))
+      .then(({url}) => {
+          console.log(`New hyperdrive published for ${project}`);
+
+          // Update DNS record
+          let hyperdriveKey = url.replace('hyper://', '');
+          return updateDnsRecordDigitalOcean(domain, 'TXT', txtHypercore, `datkey=${hyperdriveKey}`, 300, conf['digitalOceanAccessToken']);
         });
-        console.log(`POST ${url} with data ${data}`);
-        fetch(url, { method: 'POST', headers: headers, body: data })
+  }
+  if (seed === undefined) {
+    throw new Error('Failed to get dat-seed');
+  } else {
+    return seed;
+  }
+}
+
+function updateDnsRecordDigitalOcean(domain, recordType, recordName, recordData, recordTtl, doToken) {
+  // List existing DNS records for domain
+  let url = `https://api.digitalocean.com/v2/domains/${domain}/records/`;
+  let headers = {
+    'Content-Type': 'application/json',
+    'Authorization': `Bearer ${doToken}`
+  };
+  console.log(`GET ${url}`);
+  return fetch(url, { headers: headers })
+    .then(res => res.json())
+    .then(json => json['domain_records'].filter(record => record['type'] === recordType && record['name'] === recordName))
+    .then(txt => {
+      // Delete existing records with matching record type and name
+      txt.forEach((item, index) => {
+        let urlDelete = url + item['id'];
+        console.log(`DELETE ${urlDelete}`);
+        fetch(urlDelete, { method: 'DELETE', headers: headers })
           .catch(function(error) {
             console.log(error);
           });
-      })
-      .catch(function(error) {
-        console.log(error);
       });
-   });
+
+      // Create new DNS record
+      data = JSON.stringify({
+        type: recordType,
+        name: recordName,
+        data: recordData,
+        ttl:  recordTtl
+      });
+      console.log(`POST ${url} with data ${data}`);
+      return fetch(url, { method: 'POST', headers: headers, body: data });
+    });
 }
