8547aab1cd580574f0ab19b3fa70aebe79e7defa
Mauve Signweaver <RangerMauve@mauve.moe>
2023-01-19 19:06:45 -0500

    Add initial hypercore sync code

--- v1/protocols/hyper.ts
+++ v1/protocols/hyper.ts
@@ -1,5 +1,9 @@
-import Protocol, { SyncOptions } from './interfaces'
+import * as SDK from 'hyper-sdk'
+import LocalDrive from 'localdrive'
+
 import { Static } from '@sinclair/typebox'
+
+import Protocol, { SyncOptions } from './interfaces'
 import { HyperProtocolFields } from '../api/schemas'
 
 export interface HyperProtocolOptions {
@@ -7,22 +11,57 @@ export interface HyperProtocolOptions {
 }
 
 export class HyperProtocol implements Protocol<Static<typeof HyperProtocolFields>> {
+  constructor (options: HyperProtocolOptions) {
+    this.options = options
+    this.sdk = null
+    this.drives = new Map()
+  }
+
   async load (): Promise<void> {
-    // TODO(protocol): stub
-    return await Promise.resolve()
+    const { path: storage } = this.options
+    // TODO: Where do we signal to load up all the initial sites?
+    this.sdk = await SDK.create({ storage })
+  }
+
+  async getDrive (id) {
+    if (this.drives.has(id)) {
+      return this.drives.get(id)
+    }
+    const drive = await this.sdk.getDrive(id)
+    this.drives.set(id, drive)
+
+    drive.once('close', () => {
+      this.drives.delete(id)
+    })
+
+    return drive
   }
 
   async sync (id: string, folderPath: string, options?: SyncOptions): Promise<Static<typeof HyperProtocolFields>> {
-    // TODO(protocol): stub
+    const drive = await this.getDrive(id)
+    const fs = new LocalDrive(folderPath)
+
+    const mirror = fs.mirror(drive)
+
+    // TODO: Should we log the changes somewhere like IPFS?
+    await mirror.done()
+
+    const raw = drive.url
+    const link = raw
+    // TODO: Pass in the gateway URL in the config first
+    const gateway = 'oops'
+
     return {
       enabled: true,
-      link: 'example-link',
-      gateway: 'example-gateway',
-      raw: 'example-raw'
+      link,
+      gateway,
+      raw
     }
   }
 
   async unsync (site: Static<typeof HyperProtocolFields>): Promise<void> {
-    return await Promise.resolve()
+    const drive = await this.getDrive(id)
+    // TODO: Should we also clear the stored data?
+    await drive.close()
   }
 }
