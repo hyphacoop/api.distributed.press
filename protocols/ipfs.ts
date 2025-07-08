9f9264ebd8f35871fc46c17c917e0ef78a2cc04b
Akhilesh Thite <akhithite@gmail.com>
2025-07-08 13:06:58 -0700

    chore: disable webRTC direct for testing

--- protocols/ipfs.ts
+++ protocols/ipfs.ts
@@ -90,7 +90,7 @@ export class IPFSProtocol implements Protocol<Static<typeof IPFSProtocolFields>>
   ipns: any | null
 
   constructor (options: IPFSProtocolOptions) {
-    this.options = { ...options, useWebRTC: options.useWebRTC ?? true }
+    this.options = { ...options, useWebRTC: options.useWebRTC ?? false }
     this.onCleanup = []
     this.helia = null
     this.ipfsFs = null
